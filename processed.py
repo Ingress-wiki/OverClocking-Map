669f3fb1fef0abbc7e42de0c3b4992051dd3223f
ChestnutLUO <digby@65mail.cn>
2023-12-07 12:34:59 +0800

    repush

--- processed.py
+++ processed.py
@@ -17,10 +17,10 @@ with open(file_path, 'r') as infile:
         writer.writerow(new_header)
 
         for row in reader:
-            new_row = [row[i] for i in range(len(row)) if i not in indices_to_remove]
-            writer.writerow(new_row)
-            #if 'img_uri' in header and header.index('img_uri') < len(row) and row[header.index('img_uri')]:
-            #    new_row = [row[i] for i in range(len(row)) if i not in indices_to_remove]
-            #    writer.writerow(new_row)
+            #new_row = [row[i] for i in range(len(row)) if i not in indices_to_remove]
+            #writer.writerow(new_row)
+            if 'img_uri' in header and header.index('img_uri') < len(row) and row[header.index('img_uri')]:
+                new_row = [row[i] for i in range(len(row)) if i not in indices_to_remove]
+                writer.writerow(new_row)
 
 print("Processing is complete")
