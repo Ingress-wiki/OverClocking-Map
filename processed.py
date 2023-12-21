26d0f2651c5507f00351713f38207d79ffe197f2
ChestnutLUO <digby@65mail.cn>
2023-12-21 12:52:01 +0800

    add counter feature

--- processed.py
+++ processed.py
@@ -1,7 +1,8 @@
 import csv
+from datetime import datetime
 
 file_path = 'POIsLightshipDevPortal.csv'
-
+Counter = 0
 with open(file_path, 'r') as infile:
     reader = csv.reader(infile)
     header = next(reader)
@@ -9,18 +10,21 @@ with open(file_path, 'r') as infile:
     columns_to_remove = ['id', 'img_uri', 'address', 'localizability']
     indices_to_remove = [header.index(col) for col in columns_to_remove if col in header]
 
-    with open('POIdb.csv', 'w', newline='') as outfile:
+    with open('POIdb.csv', 'w', newline='') as outfile, open('count.csv', 'a', newline='') as recordfile:
         writer = csv.writer(outfile)
-
+        record_writer = csv.writer(recordfile)
 
         new_header = [col for col in header if col not in columns_to_remove]
         writer.writerow(new_header)
 
         for row in reader:
-            #new_row = [row[i] for i in range(len(row)) if i not in indices_to_remove]
-            #writer.writerow(new_row)
             if 'img_uri' in header and header.index('img_uri') < len(row) and row[header.index('img_uri')]:
                 new_row = [row[i] for i in range(len(row)) if i not in indices_to_remove]
                 writer.writerow(new_row)
+                Counter += 1
+
+        # Output timestamp and counter to record.csv
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        record_writer.writerow([timestamp, Counter])
 
 print("Processing is complete")
